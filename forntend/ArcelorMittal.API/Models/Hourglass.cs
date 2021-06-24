c067abe4ed837e8b46dc3f654143195686284b50
ceyhunmavzer <64132737+ceyhunmavzer@users.noreply.github.com>
2021-06-24 12:32:55 +0200

    initial commit

---
+++ forntend/ArcelorMittal.API/Models/Hourglass.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace ArcelorMittal.API.Models
+{
+    public class Hourglass
+    {
+        public List<List<int>> Data { get; set; }
+    }
+}
---
+++ forntend/ArcelorMittal.API/Models/Sum.cs
@@ -0,0 +1,7 @@
+namespace ArcelorMittal.API.Models
+{
+    public class SumResponse
+    {
+        public int Response { get; set; }
+    }
+}
